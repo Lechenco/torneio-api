ceee07311965c50a0bee23f3ef725b30f222b5a4
Gabriel Lechenco <glechenco@gmail.com>
2026-04-20 18:45:54 +0000

    first commit

---
+++ src/main/java/io/github/lechenco/tier/TierApplication.java
@@ -0,0 +1,13 @@
+package io.github.lechenco.tier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierApplication.class, args);
+	}
+
+}
