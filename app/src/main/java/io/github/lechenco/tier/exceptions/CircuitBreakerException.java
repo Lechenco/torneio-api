bddc9d3e4b6d50d24393e4fe54cf446bad7c429a
Lechenco <glechenco@gmail.com>
2026-06-11 14:25:40 -0300

    refactor: remove unused classes
    Clean repository and remove Circuit Brekaer related classes

--- app/src/main/java/io/github/lechenco/tier/exceptions/CircuitBreakerException.java
+++
@@ -1,9 +0,0 @@
-package io.github.lechenco.tier.exceptions;
-
-public class CircuitBreakerException extends Exception {
-
-    public CircuitBreakerException(String message) {
-        super(message);
-    }
-    
-}
--- app/src/main/java/io/github/lechenco/tier/exceptions/OpenCircuitBreakerException.java
+++
@@ -1,8 +0,0 @@
-package io.github.lechenco.tier.exceptions;
-
-public class OpenCircuitBreakerException extends Exception{
-
-    public OpenCircuitBreakerException(String message) {
-        super(message);
-    }
-}
